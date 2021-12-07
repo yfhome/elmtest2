7ef8639b912d319f98de8a23082887fa4828a537
yfhome <95655969+yfhome@users.noreply.github.com>
2021-12-08 00:16:52 +0200

    Part 3 - Users
    Part 3 - Users
    ●	Create a new user:
    ○	Role: editor
    ○	Username: wp-test
    ○	Password: 123456789
    ○	Email: wptest@elementor.com
    ●	Disable wp admin bar for this user, using code

--- twentytwenty-child/functions.php
+++ twentytwenty-child/functions.php
@@ -1,9 +1,15 @@
 <?php
 
-	add_action( 'wp_enqueue_scripts', 'tt_child_enqueue_parent_styles' );
+add_action( 'wp_enqueue_scripts', 'tt_child_enqueue_parent_styles' );
 
-	function tt_child_enqueue_parent_styles() {
-	   wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+function tt_child_enqueue_parent_styles() {
+   wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+}
+
+add_action('after_setup_theme', 'remove_admin_bar_for_wp_user');
+function remove_admin_bar_for_wp_user() {
+	if (wp_get_current_user()->user_login == 'wp-test') {
+		show_admin_bar(false);
 	}
-
+}
 ?>
